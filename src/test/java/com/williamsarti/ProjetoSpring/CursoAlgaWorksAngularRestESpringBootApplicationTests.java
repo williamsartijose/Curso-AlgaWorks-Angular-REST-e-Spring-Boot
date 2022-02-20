4d11aa01dd73abb6534b0ad339def9313e81dc62
williamsartijose <williamsartijose@hotmail.com>
2022-02-20 07:22:59 -0300

    Iniciando o Projeto

---
+++ src/test/java/com/williamsarti/ProjetoSpring/CursoAlgaWorksAngularRestESpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamsarti.ProjetoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoAlgaWorksAngularRestESpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
