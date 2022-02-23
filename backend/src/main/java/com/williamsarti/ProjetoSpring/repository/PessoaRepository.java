b5fa252739845d5cf391b8bfc5ffe225f6a045c7
williamsartijose <williamsartijose@hotmail.com>
2022-02-23 17:41:19 -0300

    Back-End Java API Rest - spring boot   -   ClasseRepository PessoaRepository

--- backend/src/main/java/com/williamsarti/ProjetoSpring/repository/PessoaRepository.java
+++ backend/src/main/java/com/williamsarti/ProjetoSpring/repository/PessoaRepository.java
@@ -1,14 +1,12 @@
 package com.williamsarti.ProjetoSpring.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.williamsarti.ProjetoSpring.model.Pessoa;
 
-@Repository
-public interface PessoaRepository
-    extends
-        JpaRepository<Pessoa,Long>
-{
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    
 }
