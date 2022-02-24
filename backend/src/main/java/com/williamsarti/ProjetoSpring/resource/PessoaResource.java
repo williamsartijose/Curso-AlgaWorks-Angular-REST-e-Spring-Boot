bfe9b67692f7c7c0c49c2d1250c8876061c2a8a7
williamsartijose <williamsartijose@hotmail.com>
2022-02-24 11:09:07 -0300

    Back-End Java API Rest - spring boot - Usando eventos para adicionar header Location

--- backend/src/main/java/com/williamsarti/ProjetoSpring/resource/PessoaResource.java
+++ backend/src/main/java/com/williamsarti/ProjetoSpring/resource/PessoaResource.java
@@ -1,17 +1,20 @@
 package com.williamsarti.ProjetoSpring.resource;
 
-import java.net.URI;
-
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import com.williamsarti.ProjetoSpring.repository.PessoaRepository;
-import com.williamsarti.ProjetoSpring.model.Pessoa;
 
+import com.williamsarti.ProjetoSpring.event.RecursoCriadoEvent;
+import com.williamsarti.ProjetoSpring.model.Categoria;
+import com.williamsarti.ProjetoSpring.model.Pessoa;
+import com.williamsarti.ProjetoSpring.repository.PessoaRepository;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +22,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -28,20 +30,23 @@ public class PessoaResource {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
-		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);		
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
-			response.setHeader("Location", uri.toASCIIString());
-			
-			return ResponseEntity.created(uri).body(pessoaSalva);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
-		final Pessoa pessoa = pessoaRepository.findOne(codigo);
-		return pessoa !=null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+		 Pessoa pessoa = pessoaRepository.findOne(codigo);
+	 return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
 	}
+	
+
 }
