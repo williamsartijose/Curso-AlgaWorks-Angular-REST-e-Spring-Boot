cec0b24af3551c26bb879a1ca74f12c1ce0639fb
williamsartijose <williamsartijose@hotmail.com>
2022-02-28 10:49:17 -0300

    [Back-End]-Java API Rest - spring boot - [Segurança da API] Configurando JWT no projeto

--- backend/src/main/java/com/williamsarti/ProjetoSpring/config/ResourceServerConfig.java
+++ backend/src/main/java/com/williamsarti/ProjetoSpring/config/ResourceServerConfig.java
@@ -1,21 +1,25 @@
 package com.williamsarti.ProjetoSpring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig
+@EnableResourceServer
+public class ResourceServerConfig
     extends
-        WebSecurityConfigurerAdapter
+        ResourceServerConfigurerAdapter
 {
 
-    @Override
-    protected void configure(
+    @Autowired
+    public void configure(
         AuthenticationManagerBuilder auth )
         throws Exception
     {
@@ -24,7 +28,7 @@ public class SecurityConfig
     }
 
     @Override
-    protected void configure(
+    public void configure(
         HttpSecurity http )
         throws Exception
     {
@@ -32,9 +36,16 @@ public class SecurityConfig
             .antMatchers( "/categorias" ).permitAll()
             .anyRequest().authenticated()
             .and()
-            .httpBasic().and()
             .sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS ).and()
             .csrf().disable();
+    }
+
+    @Override
+    public void configure(
+        ResourceServerSecurityConfigurer resources )
+        throws Exception
+    {
+        resources.stateless( true );
     }
 
 }
