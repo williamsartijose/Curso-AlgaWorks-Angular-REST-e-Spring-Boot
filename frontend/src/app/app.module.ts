3e7aa47ae91c004ede93149b1e710e587e9787d3
williamsartijose <williamsartijose@hotmail.com>
2022-03-11 11:10:29 -0300

    [Front-End]- Angular - VisualCode - Adicionando tooltip

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { TabViewModule } from 'primeng/tabview';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { AppComponent } from './app.component';
 
@@ -19,6 +20,7 @@ import { AppComponent } from './app.component';
     InputTextModule,
     TableModule,
     TabViewModule,
+    TooltipModule
 
   ],
   providers: [],
